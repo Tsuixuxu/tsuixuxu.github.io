e615233aeb3008c14afefb17202c1073a2257b24
Junxiong.Xu <Junxiong.Xu@geely.com>
2019-03-04 15:30:47 +0800

    first

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Works from "./views/works/index.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      redirect: "works"
+    },
+    {
+      path: "/works",
+      name: "works",
+      component: Works
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    }
+  ]
+});

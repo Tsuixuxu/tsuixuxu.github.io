eb03af1ad376d3af56d0ce0b5fe64eee13d064ef
Junxiong.Xu <Junxiong.Xu@geely.com>
2019-07-17 09:57:20 +0800

    lazy loading

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import "./source/less/index.less";
 import VueLazyload from "vue-lazyload";
+import Loading from "./assets/loading.gif";
 
-Vue.use(VueLazyload);
+// Vue.use(VueLazyload);
 
 // or with options
 Vue.use(VueLazyload, {
   preLoad: 1.3,
-  loading: require("./assets/loading.gif"),
+  loading: Loading,
   attempt: 3
 });
 
